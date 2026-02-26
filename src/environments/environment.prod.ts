0ba61fd164077c2c752134b0b5bc2865191769e7
DESKTOP-T1R44MN\asus <natchapol4750@gmail.com>
2026-02-26 16:48:07 +0700

    fix: update production api url3

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://ivmdheoqjmbgceytwnlc.supabase.co',
+  supabaseKey: 'sb_publishable_p7KFVK_MWLPjJIdAjLQ5WA_hdVy5fEZ' ,
+  apiUrl: 'https://fastpassadminapi.vercel.app/api',
+};
