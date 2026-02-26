d5bc531dd5ec379b757531bb487c8cd5cdd789ac
DESKTOP-T1R44MN\asus <natchapol4750@gmail.com>
2026-02-26 16:42:55 +0700

    fix: update production api url2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
   supabaseUrl: 'https://ivmdheoqjmbgceytwnlc.supabase.co',
   supabaseKey: 'sb_publishable_p7KFVK_MWLPjJIdAjLQ5WA_hdVy5fEZ' ,
   apiUrl: 'https://fastpassadminapi.vercel.app/api',
